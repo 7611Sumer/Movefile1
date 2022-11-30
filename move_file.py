204ce1b9fd051274f477a49586489e764c74cef7
7611Sumer <87403522+7611Sumer@users.noreply.github.com>
2022-11-30 16:05:41 +0530

    Add files via upload

---
+++ move_file.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+
+from_dir = "//Users//navintojsinghminhas//Downloads"
+to_dir = "//Users//navintojsinghminhas//Desktop//DocumentFiles"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for i in ()
+
+# Mam i am not able to understand what ahead to write in the for-in loop for checking out in the files
